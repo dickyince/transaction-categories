910d4d9d31d33d315329e346e5c93a16398a35bb
dickyince <minnimince@googlemail.com>
2021-03-13 17:50:22 +0000

    Implementing tests to fulfill requirements

--- src/test/java/com/coding/exercise/FileTransactionDAOTest.java
+++ src/test/java/com/coding/exercise/FileTransactionDAOTest.java
@@ -1,6 +1,15 @@
 package com.coding.exercise;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class FileTransactionDAOTest {
 
@@ -10,5 +19,165 @@ public class FileTransactionDAOTest {
     @Before
     public void setup() {
         testSubject = new FileTransactionDAO("src/test/resources/testTransactions.json");
+    }
+
+    @Test
+    public void testGetTransactionsByCategory_null_if_file_does_not_exist() {
+        // if
+        testSubject = new FileTransactionDAO("src/test/resources/non_existent_file.json");
+        // when
+        List<Transaction> actualResult = testSubject.getTransactionsByCategory("Groceries");
+        // then
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void testGetTransactionsByCategory_null_if_json_malformed() {
+        // if
+        testSubject = new FileTransactionDAO("src/test/resources/malformedTransactions.json");
+        // when
+        List<Transaction> actualResult = testSubject.getTransactionsByCategory("Groceries");
+        // then
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void testGetTransactionsByCategory_Success_for_Groceries_category() {
+        // if
+        List<Transaction> expectedResult = TestHelper.getGroceriesTransactions();
+        // when
+        List<Transaction> actualResult = testSubject.getTransactionsByCategory("Groceries");
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetTransactionsByCategory_Success_for_null_category() {
+        // if
+        List<Transaction> expectedResult = TestHelper.getNullTransactions();
+        // when
+        List<Transaction> actualResult = testSubject.getTransactionsByCategory(null);
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetTotalsForCategories_Success() {
+        // if
+        Map<String, BigDecimal> expectedResult = TestHelper.getCategoryTotals();
+        // when
+        Map<String, BigDecimal> actualResult = testSubject.getTotalsForCategories();
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetMonthlyAveragesByCategory_null_for_nonexistent_category() {
+        // if
+        // Expecting a null return value
+        // when
+        BigDecimal actualResult = testSubject.getMonthlyAveragesByCategory("nonexistent");
+        // then
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void testGetMonthlyAveragesByCategory_Success_for_MyMonthlyDD_category() {
+        // if
+        BigDecimal expectedResult = TestHelper.getMyMonthlyDDAverages();
+        // when
+        BigDecimal actualResult = testSubject.getMonthlyAveragesByCategory("MyMonthlyDD");
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetMonthlyAveragesByCategory_Success_for_null_category() {
+        // if
+        BigDecimal expectedResult = TestHelper.getNullAverages();
+        // when
+        BigDecimal actualResult = testSubject.getMonthlyAveragesByCategory(null);
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetHighestByCategoryAndYear_null_for_nonexistent_category() {
+        // if
+        // Expecting a null return value
+        // when
+        Transaction actualResult = testSubject.getHighestByCategoryAndYear("nonexistent", 2020);
+        // then
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void testGetHighestByCategoryAndYear_null_for_year_with_not_transactions() {
+        // if
+        // Expecting a null return value
+        // when
+        Transaction actualResult = testSubject.getHighestByCategoryAndYear("Groceries", 0);
+        // then
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void testGetHighestByCategoryAndYear_Success_for_Groceries_category() {
+        // if
+        Transaction expectedResult = TestHelper.getGroceriesHighest();
+        // when
+        Transaction actualResult = testSubject.getHighestByCategoryAndYear("Groceries", 2020);
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetHighestByCategoryAndYear_Success_for_null_category() {
+        // if
+        Transaction expectedResult = TestHelper.getNullHighest();
+        // when
+        Transaction actualResult = testSubject.getHighestByCategoryAndYear(null, 2019);
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetLowestByCategoryAndYear_null_for_nonexistent_category() {
+        // if
+        // Expecting a null return value
+        // when
+        Transaction actualResult = testSubject.getLowestByCategoryAndYear("nonexistent", 2020);
+        // then
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void testGetLowestByCategoryAndYear_null_for_year_with_not_transactions() {
+        // if
+        // Expecting a null return value
+        // when
+        Transaction actualResult = testSubject.getLowestByCategoryAndYear("Groceries", 0);
+        // then
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void testGetLowestByCategoryAndYear_Success_for_Groceries_category() {
+        // if
+        Transaction expectedResult = TestHelper.getGroceriesLowest();
+        // when
+        Transaction actualResult = testSubject.getLowestByCategoryAndYear("Groceries", 2020);
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
+    }
+
+    @Test
+    public void testGetLowestByCategoryAndYear_Success_for_null_category() {
+        // if
+        Transaction expectedResult = TestHelper.getNullLowest();
+        // when
+        Transaction actualResult = testSubject.getLowestByCategoryAndYear("Groceries", 2020);
+        // then
+        assertTrue(new ReflectionEquals(expectedResult).matches(actualResult));
     }
 }
--- src/test/java/com/coding/exercise/TestHelper.java
+++ src/test/java/com/coding/exercise/TestHelper.java
@@ -1,46 +1,116 @@
 package com.coding.exercise;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestHelper {
-    public static List<Transaction> readJsonFile(String file) {
-        List<Transaction> testData = new ArrayList<>();
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/LLL/yyyy");
 
-        JSONParser parser = new JSONParser();
-        try{
-            JSONArray data = (JSONArray) parser.parse(new FileReader(file));
-            for ( Object object : data ) {
-                JSONObject json = (JSONObject) object;
-                Transaction transaction = new Transaction();
-                transaction.setDate(LocalDate.parse((CharSequence) json.get("date"), dateFormat));
-                transaction.setVendor((String) json.get("vendor"));
-                transaction.setType(TransactionType.valueOf((String) json.get("type")));
-                transaction.setAmount(new BigDecimal((String) json.get("amount")).setScale(2, RoundingMode.DOWN));
-                transaction.setCategory((String) json.get("category"));
-                testData.add(transaction);
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static List<Transaction> getGroceriesTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
 
-        return testData;
+        Transaction transaction = new Transaction();
+        transaction.setDate(LocalDate.of(2020, 11, 1));
+        transaction.setVendor("Morrisons");
+        transaction.setType(TransactionType.CARD);
+        transaction.setAmount(new BigDecimal("10.40"));
+        transaction.setCategory("Groceries");
+        transactions.add(transaction);
+
+        Transaction transaction2 = new Transaction();
+        transaction2.setDate(LocalDate.of(2020, 10, 1));
+        transaction2.setVendor("M&S");
+        transaction2.setType(TransactionType.CARD);
+        transaction2.setAmount(new BigDecimal("5.99"));
+        transaction2.setCategory("Groceries");
+        transactions.add(transaction2);
+
+        return transactions;
+    }
+
+    public static List<Transaction> getNullTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
+
+        Transaction transaction = new Transaction();
+        transaction.setDate(LocalDate.of(2020, 9, 30));
+        transaction.setVendor("McMillan");
+        transaction.setType(TransactionType.INTERNET);
+        transaction.setAmount(new BigDecimal("10.00"));
+        transactions.add(transaction);
+
+        Transaction transaction2 = new Transaction();
+        transaction2.setDate(LocalDate.of(2019, 8, 27));
+        transaction2.setVendor("Random Thing");
+        transaction2.setType(TransactionType.INTERNET);
+        transaction2.setAmount(new BigDecimal("7.94"));
+        transactions.add(transaction2);
+
+        Transaction transaction3 = new Transaction();
+        transaction3.setDate(LocalDate.of(2019, 3, 19));
+        transaction3.setVendor("Awesome Hotels");
+        transaction3.setType(TransactionType.CARD);
+        transaction3.setAmount(new BigDecimal("250.94"));
+        transactions.add(transaction3);
+
+        return transactions;
+    }
+
+    public static Map<String, BigDecimal> getCategoryTotals() {
+        Map<String, BigDecimal> totals = new HashMap<>();
+
+        totals.put("MyMonthlyDD", new BigDecimal("640.00"));
+        totals.put("Groceries", new BigDecimal("16.39"));
+        totals.put(null, new BigDecimal("268.43"));
+
+        return totals;
+    }
+
+    public static BigDecimal getMyMonthlyDDAverages() {
+        return new BigDecimal("320.00");
+    }
+
+    public static BigDecimal getNullAverages() {
+        return new BigDecimal("89.47");
+    }
+
+    public static Transaction getGroceriesHighest() {
+        Transaction transaction = new Transaction();
+        transaction.setDate(LocalDate.of(2020, 11, 1));
+        transaction.setVendor("Morrisons");
+        transaction.setType(TransactionType.CARD);
+        transaction.setAmount(new BigDecimal("10.40"));
+        transaction.setCategory("Groceries");
+        return transaction;
+    }
+
+    public static Transaction getNullHighest() {
+        Transaction transaction = new Transaction();
+        transaction.setDate(LocalDate.of(2019, 3, 19));
+        transaction.setVendor("Awesome Hotels");
+        transaction.setType(TransactionType.CARD);
+        transaction.setAmount(new BigDecimal("250.49"));
+        return transaction;
+    }
+
+    public static Transaction getGroceriesLowest() {
+        Transaction transaction = new Transaction();
+        transaction.setDate(LocalDate.of(2020, 10, 1));
+        transaction.setVendor("M&S");
+        transaction.setType(TransactionType.CARD);
+        transaction.setAmount(new BigDecimal("5.99"));
+        transaction.setCategory("Groceries");
+        return transaction;
+    }
+
+    public static Transaction getNullLowest() {
+        Transaction transaction = new Transaction();
+        transaction.setDate(LocalDate.of(2019, 8, 27));
+        transaction.setVendor("Random Thing");
+        transaction.setType(TransactionType.INTERNET);
+        transaction.setAmount(new BigDecimal("7.94"));
+        return transaction;
     }
 }
